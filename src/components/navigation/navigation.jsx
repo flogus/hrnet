54e30068ab3cf4e0e0a1019a39382ef7c5f99c8a
flogus <fg@motionery.com>
2023-04-02 10:23:16 +0200

    fix: add the tab-nav

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -1,22 +1,55 @@
-import React from 'react'
+import * as React from 'react';
 import './navigation.css'
+
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 
 import { Link } from 'react-router-dom'
 
-export default function navigation () {
+
+
+function LinkTab(props) {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/create'>Create</Link>
-        </li>
-        <li>
-          <Link to='/list'>List</Link>
-        </li>
-      </ul>
-    </nav>
+    <Tab
+      component="a"
+      onClick={(event) => {
+        event.preventDefault();
+      }}
+      {...props}
+    />
+  );
+}
+
+export default function navigation () {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div>
+      <Box sx={{ width: '100%' }}>
+        <Tabs aria-label="nav tabs example">
+          <LinkTab label="Home" href="/" />
+          <LinkTab label="Create" href="/create" />
+          <LinkTab label="List" href="/list" />
+        </Tabs>
+      </Box>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/create'>Create</Link>
+          </li>
+          <li>
+            <Link to='/list'>List</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   )
 }
