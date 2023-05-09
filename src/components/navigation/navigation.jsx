7b81ecdb51b6915634af4d79a7762b67850d2737
flogus <fg@motionery.com>
2023-05-09 08:06:39 +0200

    feat: navigation : add active li on click

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -19,23 +19,37 @@ function LinkTab (props) {
 }
 
 export default function navigation () {
-  const [value, setValue] = React.useState(0)
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue)
-  }
+  const [active, setActive] = React.useState(0)
 
   return (
     <nav>
       <ul>
         <li>
-          <Link to='/'>Home</Link>
+          <Link
+            to='/'
+            onClick={() => setActive(0)}
+            className={`${active == 0 && 'active'}`}
+          >
+            Home
+          </Link>
         </li>
         <li>
-          <Link to='/create'>Create</Link>
+          <Link
+            to='/create'
+            onClick={() => setActive(1)}
+            className={`${active == 1 && 'active'}`}
+          >
+            Create
+          </Link>
         </li>
         <li>
-          <Link to='/list'>List</Link>
+          <Link
+            to='/list'
+            onClick={() => setActive(2)}
+            className={`${active == 2 && 'active'}`}
+          >
+            List
+          </Link>
         </li>
       </ul>
     </nav>
