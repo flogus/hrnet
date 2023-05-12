af1aa6ed41e763e516d210307b620ea17eef54a8
flogus <fg@motionery.com>
2023-05-12 13:14:31 +0200

    feat: add image height

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -5,7 +5,7 @@ export default function home () {
   return (
     <div className='container'>
       <h2>welcome to HRnet</h2>
-      <img src={logo} alt='Wealth Health' width='300px' />
+      <img src={logo} alt='Wealth Health' width='300px' height='276px' />
     </div>
   )
 }
